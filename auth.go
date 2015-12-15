e7412cccd9a2161f1ea65636c6c3007287194ed3
Andrew Burian <andrew@burian.ca>
2015-12-14 18:00:01 -0800

    Split session management into helpers
    Session is now a map[string]string which is converted
    to JSON and stored encrypted in the cookie.
    Helper functions added to avoid endless code repeat.

--- auth.go
+++ auth.go
@@ -1,26 +1,26 @@
 package main
 
 import (
-//	"encoding/json"
-	"net/http"
 	"fmt"
-//	"os"
-//	"io"
+	"github.com/andrewburian/crypter"
+	"io"
+	"net/http"
 )
 
 const (
-	BAD_REQUEST int = 400
-	NOT_FOUND int = 404
+	BAD_REQUEST  int = 400
+	NOT_FOUND    int = 404
 	UNAUTHORIZED int = 401
+	ERROR        int = 500
 )
 
-type auth struct {
-	name string `json:"username"`
-	pass string `json:"password"`
+type authHandler struct {
+	crypto *crypter.Crypter
 }
 
-func authHandler(w http.ResponseWriter, r *http.Request) {
+func (h *authHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	fmt.Println("Got auth request")
+	session := getSession(r, h.crypto)
 
 	// parse the form fields
 	err := r.ParseForm()
@@ -30,14 +30,25 @@ func authHandler(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	if r.PostFormValue("username") == "admin" && r.PostFormValue("password") == "pass" {
-		fmt.Println("Welcome admin!")
-		w.Write([]byte(`{"autentication":"success"}`))
-	} else {
+	if len(r.FormValue("password")) < 6 {
 		w.WriteHeader(UNAUTHORIZED)
-		fmt.Println(r.PostForm)
+		io.WriteString(w, "Password must be at least 6 characters")
 	}
 
+	// TODO DB auth
+	auth := true
+
+	// failed to login
+	if !auth {
+		w.WriteHeader(UNAUTHORIZED)
+		w.Write([]byte("Username or Password incorrect"))
+		return
+	}
+
+	session["uname"] = r.FormValue("username")
+
+	setSession(w, h.crypto, session)
+
+	io.WriteString(w, "Welome")
 	return
 }
-
--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"github.com/andrewburian/crypter"
 	"net/http"
 )
 
@@ -10,10 +11,20 @@ func main() {
 	// static content
 	http.Handle("/", http.FileServer(http.Dir("./views/")))
 
-	// REST endpoints
-	http.HandleFunc("/auth", authHandler)
+	// Setup crypto
+	key := []byte("cryptokey0123456")
+	mac := []byte("cryptomackey")
+	iv := []byte("1234567890123456")
+
+	crypto, err := crypter.NewCrypter(key, mac, iv)
+	if err != nil {
+		panic(err)
+	}
+
+	// REST endpoint handlers
+	auth := &authHandler{crypto}
+	http.Handle("/auth", auth)
 
 	fmt.Println("Starting server!")
 	http.ListenAndServe(":8080", nil)
 }
-
